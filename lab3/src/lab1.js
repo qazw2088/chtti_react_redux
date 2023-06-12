7000d8b2934772eae640782962c8035e07f10ffb
unknown <chtti@DESKTOP-A08GUP1.cdsafi.com>
2023-06-12 14:11:09 +0800

    set babel behavior in .babelrc

--- lab3/src/lab1.js
+++ lab3/src/lab1.js
@@ -1,8 +1,8 @@
-let x = 5
-console.log("to begin with, x=",x)
-x+=1
-console.log("now, x=",x);
-const y = 'hello world, x=${x}'
+let x = 5;
+console.log("to begin with, x=", x);
+x += 1;
+console.log("now, x=", x);
+const y = "hello world, x=${x}";
 console.log(y);
-const z = `hello world, x=${x}`
+const z = `hello world, x=${x}`;
 console.log(z);
---
+++ lab3/src/lab2.js
@@ -0,0 +1,5 @@
+"use strict";
+let x = 500
+console.log(x)
+x1 = 600
+console.log(x)
