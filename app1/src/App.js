3da92c82a259214ea95e0649ea04a460d79517f8
Wei <qazw2088@gmail.com>
2023-06-13 15:08:26 +0800

    feat: state

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,22 +1,54 @@
 import { Component } from "react";
-
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Person from "./components/Person";
-import Pets from "./components/Pets";
-
+import Pet from "./components/Pet";
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Mark", age: 46 },
+      { name: "James", age: 39 },
+      { name: "Tim", age: 35 },
+      { NAME: "kEVIN", AGE: 50 },
+    ],
+  };
+  changeNameHandler = () => {
+    console.log("button is clicked");
+    //this.state.persons[0].name = "Captain America"
+    this.setState({
+      persons: [
+        { name: "Captain America", age: 38 },
+        { name: "Thor", age: 37 },
+        { name: "Iron Man", age: 35 },
+        { NAME: "Hawk", AGE: 50 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <Dashboard />
-        <Person name="Mark" age="46" />
-        <Pets name="King" specie="cat"></Pets>
-        <Person name="James" age="39" />
-        <Person name="Tim" age="35">
+        <button onClick={this.changeNameHandler}>Change</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Pet name="King" specie="cat" />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
           Team Leader
         </Person>
-        <Person name="Kevin" age="50" />
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        />
       </div>
     );
   }
--- app1/src/components/Pet.jsx
+++ app1/src/components/Pet.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Pets extends Component {
+export default class Pet extends Component {
   render() {
     return (
       <div>
