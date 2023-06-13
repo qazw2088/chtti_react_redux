91ff85418d791deaaabc7ea031d1a63590973e70
Wei <qazw2088@gmail.com>
2023-06-13 11:27:24 +0800

    feat: use class component to create element

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,15 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>this is header2</h2>
-      </header>
-      <h1>begin body</h1>
-    </div>
-  );
+import "./App.css";
+class App extends Component {
+  render() {
+    console.log("App render called");
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+        <h1>I am Mark</h1>
+      </div>
+      // React.createElement('div',{className:'App'},React.createElement('h1', null,'Hi'),
+      // React.createElement('h1',null,'I am Mark'))
+    );
+  }
 }
 
 export default App;
