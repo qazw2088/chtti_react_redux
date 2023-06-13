c377158c747409a5ace83de84460b55cb40c706d
Wei <qazw2088@gmail.com>
2023-06-13 15:27:45 +0800

    feat: useState in react functional component

--- app1/src/components/Person.jsx
+++ app1/src/components/Person.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Person = (props) => {
+  // let publisher = "marvel studio";
+  const [vendor, setVendor] = useState({ publisher: "marvel studio" });
+  const switchVendorHandler = () => {
+    console.log("original publisher=", vendor.publisher);
+    // vendor.publisher = "DC commic"; //WRONG!!!!
+    setVendor({ publisher: "DC commic" });
+    console.log("after change, publisher=", vendor.publisher);
+  };
   console.log(`inside Person, prpos.name=${props.name}`);
   return (
     <div>
+      <h3>Publisher={vendor.publisher}</h3>
+      <button onClick={switchVendorHandler}>Change league</button>
       <p>I am {props.name}</p>
       <p>I am {props.age} years old</p>
       <p style={{ color: "red", fontWeight: "bold" }}>
