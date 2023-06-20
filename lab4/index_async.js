a20d101d5bb79290f8097f8e67b3e27c13012bb5
Wei <qazw2088@gmail.com>
2023-06-20 14:40:13 +0800

    feat: create store, action creator

---
+++ lab4/index_async.js
@@ -0,0 +1,54 @@
+const redux = require("redux");
+const ReduxLogger = require("redux-logger");
+const logger = ReduxLogger.createLogger();
+// initial state
+const initState = {
+    loading: false,
+    users: [],
+    error: "",
+};
+// action
+const FETCH_USER_REQUEST = "FETCH_USER_REQUEST";
+const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+// sync action creator
+const fetchUserRequest = () => {
+    return { type: FETCH_USER_REQUEST };
+};
+const fetchUserSuccess = (users) => {
+    return {
+        type: FETCH_USER_SUCCESS,
+        payload: users,
+    };
+};
+const fetchUserFailure = (error) => {
+    return {
+        type: FETCH_USER_FAILURE,
+        payload: error,
+    };
+};
+
+// reducer
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        case FETCH_USER_REQUEST:
+            return { ...state, loading: true };
+        case FETCH_USER_SUCCESS:
+            return { loading: false, users: action.payload, error: "" };
+        case FETCH_USER_FAILURE:
+            return { loading: false, users: [], error: action.payload };
+        default:
+            return state;
+    }
+};
+
+const store = redux.createStore(reducer, redux.applyMiddleware(logger));
+console.log("init state=", store.getState());
+// initial a http request
+store.dispatch(fetchUserRequest());
+// communicate with server, and success get users
+store.dispatch(fetchUserSuccess(["Mark", "Jean", "d"]));
+// initial a http request
+store.dispatch(fetchUserRequest());
+// communicate with server, and fail to get users, get some error messages
+store.dispatch(fetchUserFailure("wrong request parameter, id=xxxx not found"));
