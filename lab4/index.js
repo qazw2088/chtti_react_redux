79a759a61ab6f09529e2b0998e2914c5f4465ece
Wei <qazw2088@gmail.com>
2023-06-20 13:54:30 +0800

    feat: apply middleware

--- lab4/index.js
+++ lab4/index.js
@@ -1,7 +1,10 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
 console.log(typeof redux);
 const createStore = redux.createStore;
-
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 // action type
 const BUY_COKE = "BUY_COKE";
 const BUY_FANTA = "BUT_FANTA";
@@ -20,21 +23,31 @@ function buyFanta() {
 }
 
 // multiple state
-const initialState = { numberOfCokes: 10, numberOfFantas: 20 };
-// reducer
-const reducer = (state = initialState, action) => {
+const initalCokeState = { numberOfCokes: 10 };
+const initalFantaState = { numberOfFantas: 20 };
+// sperate reducer
+const cokeReducer = (state = initalCokeState, action) => {
     //console.log("reducer process:",action.type, ",", action.info)
     switch (action.type) {
         case BUY_COKE:
             return { ...state, numberOfCokes: state.numberOfCokes - 1 };
+        default:
+            return state;
+    }
+};
+
+const fantaReducer = (state = initalFantaState, action) => {
+    //console.log("reducer process:",action.type, ",", action.info)
+    switch (action.type) {
         case BUY_FANTA:
             return { ...state, numberOfFantas: state.numberOfFantas - 1 };
         default:
             return state;
     }
 };
+const rootReducer = combineReducers({ coke: cokeReducer, fanta: fantaReducer });
 // store
-const store = createStore(reducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("initial state=", store.getState());
 const unsubscribe = store.subscribe(() => {
     console.log("store updated as:", store.getState());
