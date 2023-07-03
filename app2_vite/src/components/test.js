b9f3ff5a61c83ef0ee08194115e202ae8d3d49ea
Wei Xuan <leocai@cht.com.tw>
2023-07-03 14:47:15 +0800

    refactor: Remove unused code from test.js

--- app2_vite/src/components/test.js
+++ app2_vite/src/components/test.js
@@ -1,35 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
-function DoLogin(username, password){
-    const data = {
-        username: username,
-        password: password
-    };
-    return axios.post('/api/login', data);
+function DoLogin(username, password) {
+  const data = {
+    username: username,
+    password: password,
+  };
+  return axios.post("/api/login", data);
 }
 
 // export default DoLogin;
-function addTwoNumbers(num1, num2){
-    return num1 + num2;
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
 }
 
-function test_addTwoNumbers(){
-    let result = addTwoNumbers(1, 2);
-    if(result !== 3){
-        throw new Error('Expected 1 + 2 to equal 3');
-    }
+function test_addTwoNumbers() {
+  let result = addTwoNumbers(1, 2);
+  if (result !== 3) {
+    throw new Error("Expected 1 + 2 to equal 3");
+  }
 }
 
-function mergeTwoList(list1, list2){
-    return [...list1, ...list2];
+function mergeTwoList(list1, list2) {
+  return [...list1, ...list2];
 }
 
-function test_mergeTwoList(){
-    // comment out the following line to pass the test
-    // throw new Error('Expected [1, 2, 3, 4]');
+function test_mergeTwoList() {
+  // comment out the following line to pass the test
+  // throw new Error('Expected [1, 2, 3, 4]');
 
-    let result = mergeTwoList([1, 2], [3, 4]);
-    if(result.length !== 4){
-        throw new Error('Expected [1, 2, 3, 4]');
-    }
+  let result = mergeTwoList([1, 2], [3, 4]);
+  if (result.length !== 4) {
+    throw new Error("Expected [1, 2, 3, 4]");
+  }
 }
